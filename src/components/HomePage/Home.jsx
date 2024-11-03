be3512bc9a0f5bf9ea491f02fed06fed09c108fb
ahamedroshan8883.h <ahamed8883.h@gmail.com>
2024-11-03 12:30:06 +0530

    change positon of hompage elements

--- src/components/HomePage/Home.jsx
+++ src/components/HomePage/Home.jsx
@@ -45,22 +45,22 @@ export default function Home() {
       <section id="homePage" className="flex -z-10 flex-col items-center h-full justify-center bg-homebg">
         <Navbar />
         <ul className="box-area absolute overflow-hidden w-full h-full top-0 left-0">
-          <li className="w-[80px] h-[80px] sm:w-[70px] sm:h-[70px] absolute sm:top-[130px] sm:left-[25%] top-[40px] left-[10%] block animate-[animate_20s_linear_infinite]">
+          <li className="w-[80px] h-[80px] sm:w-[70px] sm:h-[70px] absolute sm:top-[130px] sm:left-[25%] top-[130px] left-[10%] block animate-[animate_20s_linear_infinite]">
             <img src={shape1} alt="shape" />
           </li>
-          <li className="w-[70px] h-[70px] absolute top-[230px] left-[15%] sm:left-[20%] sm:top-[300px] block animate-[animate_20s_linear_infinite]">
+          <li className="w-[60px] h-[60px] absolute top-[140px] left-[80%] sm:top-[120px] sm:w-[50px] sm:h-[50px] sm:left-[60%] block animate-[animate_20s_linear_infinite]">
             <img src={shape2} alt="shape" />
           </li>
-          <li className="w-[100px] h-[100px] absolute top-[180px] left-[65%] sm:top-[270px] sm:left-[60%] block animate-[animate_20s_linear_infinite]">
+          <li className="w-[90px] h-[90px] absolute top-[350px] left-[65%] sm:top-[270px] sm:left-[60%] block animate-[animate_20s_linear_infinite]">
             <img src={shape3} alt="shape" />
           </li>
-          <li className="w-[90px] h-[90px] absolute top-[50px] left-[40%] sm:top-[60px] sm:w-[100px] sm:h-[100px] block animate-[animate_20s_linear_infinite]">
+          <li className="w-[90px] h-[90px] absolute top-[100px] left-[40%] sm:top-[60px] sm:w-[100px] sm:h-[100px] block animate-[animate_20s_linear_infinite]">
             <img src={shape4} alt="shape" />
           </li>
-          <li className="w-[40px] h-[40px] absolute top-[80px] left-[80%] sm:top-[120px] sm:w-[50px] sm:h-[50px] sm:left-[60%] block animate-[animate_20s_linear_infinite]">
+          <li className="w-[60px] h-[60px] absolute top-[350px] left-[15%] sm:left-[20%] sm:top-[300px] block animate-[animate_20s_linear_infinite]">
             <img src={shape5} alt="shape" />
           </li>
-          <li className="w-[110px] h-[110px] absolute top-[270px] left-[34%] sm:top-[300px]  block animate-[animate_20s_linear_infinite]">
+          <li className="w-[100px] h-[100px] absolute top-[270px] left-[34%] sm:top-[300px]  block animate-[animate_20s_linear_infinite]">
             <img src={shape6} alt="shape" />
           </li>
         </ul>
@@ -72,7 +72,7 @@ export default function Home() {
           </h1>
         </div>
         <div className=" bg-white z-10 rounded-tl-3xl rounded-tr-3xl w-full 
-        model-type-icons box-border  h-[30vh] px-2 py-4 flex flex-wrap justify-center gap-3 sm:gap-16">
+        model-type-icons box-border  h-[30vh] px-1 py-4 flex flex-wrap justify-center gap-1 sm:gap-16">
           {software.map(item=>
             <div className="software-det flex flex-col items-center " key={item.name}>
               <div className="img hover:saturate-50 cursor-pointer" >
