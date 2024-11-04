863a559a193693b59d89a262b422191f8d103dc4
ahamedroshan8883.h <ahamed8883.h@gmail.com>
2024-11-04 23:17:38 +0530

    change background

--- src/components/HomePage/Home.jsx
+++ src/components/HomePage/Home.jsx
@@ -51,15 +51,15 @@ export default function Home() {
         <div className="mainContent relative z-10 h-[75vh] w-full justify-center flex items-center">
         <ul className="box-area -z-20 overflow-hidden w-full h-full absolute left-0 top-0">
           {/* 1 */}
-          <li className="absolute w-[25vw] h-[25vw] sm:w-[13vw] sm:h-[13vh] sm:top-[25vh] sm:left-[7vw] top-[10vh] left-[10vw] opacity-50 block">
+          <li className="absolute w-[25vw] h-[25vw] sm:w-[13vw] sm:h-[13vh] sm:top-[25vh] sm:left-[7vw] top-[10vh] left-[10vw] opacity-70 block">
             <img src={shape9} alt="shape" />
           </li>
           {/* 2 */}
-          <li className="absolute w-[20vw] h-[20vh] top-[10vh] left-[75vw] sm:w-[12vw] sm:h-[12vh] sm:top-[25vh] sm:left-[80vw] opacity-50 block">
+          <li className="absolute w-[20vw] h-[20vh] top-[10vh] left-[75vw] sm:w-[12vw] sm:h-[12vh] sm:top-[25vh] sm:left-[80vw] opacity-70 block">
             <img src={shape7} alt="shape" />
           </li>
           {/* 3 */}
-          <li className="absolute w-[20vw] h-[20vw] top-[55vh] left-[75vw] sm:w-[10vw] sm:h-[10vh] sm:left-[70vw] sm:top-[45vh] opacity-50 block">
+          <li className="absolute w-[20vw] h-[20vw] top-[55vh] left-[75vw] sm:w-[10vw] sm:h-[10vh] sm:left-[70vw] sm:top-[45vh] opacity-70 block">
             <img src={shape3} alt="shape" />
           </li>
           {/* 4 */}
@@ -67,15 +67,15 @@ export default function Home() {
             <img src={shape4} alt="shape" />
           </li>
           {/* 5 */}
-          <li className="absolute w-[20vw] h-[20vw] top-[55vh] left-[22vw] sm:h-[11vh] sm:w-[11vw] sm:left-[20vw] sm:top-[45vh] opacity-50 block">
+          <li className="absolute w-[20vw] h-[20vw] top-[55vh] left-[22vw] sm:h-[11vh] sm:w-[11vw] sm:left-[20vw] sm:top-[45vh] opacity-70 block">
             <img src={shape5} alt="shape" />
           </li>
           {/* 6 */}
-          <li className="absolute w-[15vw] h-[15vw] top-[42vh] left-[30vw] sm:h-[12vh] sm:w-[12vw] sm:left-[37vw] sm:top-[55vh] opacity-50 block">
+          <li className="absolute w-[15vw] h-[15vw] top-[42vh] left-[30vw] sm:h-[12vh] sm:w-[12vw] sm:left-[37vw] sm:top-[55vh] opacity-70 block">
             <img src={shape6} alt="shape" />
           </li>
           {/* 7 */}
-          <li className="absolute w-[15vw] h-[15vw] top-[45vh] left-[60vw] sm:w-[12vw] sm:h-[12vh] sm:top-[55vh] sm:left-[55vw] md:w-[7vw] md:h-[7vw] opacity-50 block">
+          <li className="absolute w-[15vw] h-[15vw] top-[45vh] left-[60vw] sm:w-[12vw] sm:h-[12vh] sm:top-[55vh] sm:left-[55vw] md:w-[7vw] md:h-[7vw] opacity-70 block">
             <img src={shape8} alt="shape" />
           </li>
         </ul>
--- tailwind.config.js
+++ tailwind.config.js
@@ -15,8 +15,8 @@ module.exports = {
         profileBoxShadow: 'rgba(0, 0, 0, 0.25) 0px 54px 55px, rgba(0, 0, 0, 0.12) 0px -12px 30px, rgba(0, 0, 0, 0.12) 0px 4px 6px, rgba(0, 0, 0, 0.17) 0px 12px 13px, rgba(0, 0, 0, 0.09) 0px -3px 5px',
       },
        backgroundImage: {
-        'homebg' : 'linear-gradient(to bottom, #19191a, #222127, #2e2933, #3b313f, #4a384a)',
-        // 'homebg': 'radial-gradient(circle, #3a4c7a, #2f4172, #24366a, #182b62, #0a205a)',
+        // 'homebg' : 'linear-gradient(to bottom, #19191a, #222127, #2e2933, #3b313f, #4a384a)',
+        'homebg': 'radial-gradient(circle, #3a4c7a, #2f4172, #24366a, #182b62, #0a205a)',
         // 'homebg': 'url("C:/Users/USER/Desktop/MeshCraft/meshcraft/src/assets/background8.jpg")',
         'wordsHover':'linear-gradient(to right, #fbb040, #faa137, #f99230, #f7822b, #f57229, #f46728, #f25c27, #f04f27, #ef4526, #ef3b25, #ee2e24, #ed1c24)',
         'custom-gradient': 'radial-gradient(circle, #f46728, #bb4434, #7d2c30, #401a21, #000000)',
